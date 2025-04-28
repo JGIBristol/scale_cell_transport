cbe0f66f3a9be807e5b8b39c041faf118e9d428a
Richard Lane <richard.lane@bristol.ac.uk>
2025-04-28 13:45:11 +0100

    get video dir

--- src/scale_cell_transport/files.py
+++ src/scale_cell_transport/files.py
@@ -4,9 +4,29 @@ Where files live etc.
 
 import pathlib
 
+from . import util
+
 
 def config_file() -> pathlib.Path:
     """
     Get the path to the config file.
     """
     return pathlib.Path(__file__).parents[2] / "config.yml"
+
+
+def rdsf_path() -> pathlib.Path:
+    """
+    Path to the RDSF directory (i.e. the mount location in the config file)
+    """
+    return pathlib.Path(util.config()["rdsf_dir"])
+
+
+def incucyte_video_dir_1() -> pathlib.Path:
+    """
+    Path to directory containing some scale videos
+
+    This lives on the RDSF, so it needs to be mounted and the mount location
+    specified in the config file.
+
+    """
+    return rdsf_path() / "Jérémie" / "Scale Culture" / "video Incucyte for Rich"
